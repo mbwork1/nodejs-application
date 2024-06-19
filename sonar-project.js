f1ae8d7a5d3ac7a45055cedb2af0ecda8dff030a
mbwork1 <mbwork1@yahoo.com>
2024-06-19 18:02:40 -0400

    Update sonar-project.js
    sonarqube update

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.236.129.45:9000/',
+  serverUrl: 'http://3.218.106.254:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': '3e9ee4f64a40359b3f9c904bc34392412e2c9d80',
+	    'sonar.login': '350f35a1335e2170ad112d910b56416bc042d6ba',
+	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
